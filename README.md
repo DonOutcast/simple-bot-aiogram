b501b092b56f6a8777921293867524bedb671c46
DonOutcast <sham1996@yandex.ru>
2023-04-19 19:58:35 +0300

    typed weather_router in new module weather.py

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-## Bot
-![Don Bot](misc/cron1.png)
+# onboarding_bot
---
+++ src/telegram_bot/model/handlers/weather.py
@@ -0,0 +1,60 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart
+from aiogram.types import Message, Location
+from aiogram.types.keyboard_button import KeyboardButton
+from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
+from aiogram.fsm.context import FSMContext
+
+from model.template.templates import RenderTemplate
+
+from model.fsm.coordinates import CoordinatesStates
+from model.keyboards.core_buttons import generate_keyboard
+
+render = RenderTemplate()
+
+weather_router = Router()
+weather_menu_buttons = generate_keyboard(
+    [
+        [
+            "Локация location",
+            "Координаты "
+
+        ],
+        [
+            "Вернуться в главное меню"
+        ]
+
+    ],
+    request_location=True
+)
+
+headers = {"throttling_key": "default", "long_operation": "typing"}
+
+
+@weather_router.message(F.text == "Погода 🌤️", flags=headers)
+async def weather_menu(message: Message):
+    await message.reply(
+        text=render.render_template("weather.html", {"user_name": message.from_user.first_name}),
+        reply_markup=weather_menu_buttons)
+
+
+@weather_router.message(F.location)
+async def location_admin(message: Message, state: FSMContext):
+    await state.set_state(CoordinatesStates.longitude)
+    await state.update_data(longitude=message.location.longitude)
+    await state.set_state(CoordinatesStates.latitude)
+    await state.update_data(latitude=message.location.latitude)
+    data = await state.get_data()
+    await state.clear()
+
+# @admin_router.message(CoordinatesStates.longitude)
+# async def longitude_function(message: Message, state: FSMContext):
+#     await state.update_data(longitude=message.location.longitude)
+#     await state.set_state(CoordinatesStates.latitude)
+
+# @admin_router.message(CoordinatesStates.latitude)
+# async def latiude_function(message: Message, state: FSMContext):
+#     await state.update_data(latitude=message.location.longitude)
+#     data = await state.get_data()
+#     print(data.latitude)
+#     print(data.longitude)
